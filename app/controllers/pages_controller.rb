5b3ee77a4ed7fe76e94f13e488b8296f3efd2ebb
mmendesc <matheus.mendescf@gmail.com>
2018-03-31 13:31:15 -0300

    About and Curriculum page, still doing table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def strategic
+  end
+
+  def curriculum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :categories
 
   get '/topics/:id' => 'teachings#topics', as: :topics
+  get '/strategic' => "pages#strategic", as: :strategic
+  get '/curriculum' => "pages#curriculum", as: :curriculum
   root to: 'pages#index'
 end

6c0df4f3b676403942f4af170b09442f774903a0
mmendesc <matheus.mendescf@gmail.com>
2018-04-01 18:55:03 -0300

    Seed from local db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create!([
+  {kind: 0, title: "JOGOS"},
+  {kind: 0, title: "FILME/TEMATIZAÇÃO"},
+  {kind: 0, title: "SIMULAÇÃO"},
+  {kind: 0, title: "SISTEMATIZAÇÃO"},
+  {kind: 0, title: "MODELIZAÇÃO"},
+  {kind: 0, title: "DISCUSSÃO/DEBATE"},
+  {kind: 0, title: "PROBLEMATIZAÇÃO"},
+  {kind: 0, title: "DRAMATIZAÇÃO "}
+])
+Teaching.create!([
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-1-numero-2-2006.html?p=14", category_id: 1, title: "AJUDANDO A FIXAR OS CONCEITOS DE GENÉTICA", views: nil, unity: "1", topics: "5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-1-numero-2-2006.html?p=19", category_id: 1, title: "GENÉTICA REVISANDO E FIXANDO CONCEITOS", views: nil, unity: "1", topics: "5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-1-numero-2-2006.html?p=33", category_id: 4, title: "MENDEL ENROLADO NA DUPLA-HÉLICE", views: nil, unity: "1", topics: "5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-2-numero-1-2007.html?p=11", category_id: 4, title: "DOS GENES AOS FENÓTIPOS", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-4-numero-1-2009.html?p=8", category_id: 1, title: "SISTEMA SANGUÍNEO SEM MISTÉRIO: uma proposta alternativa", views: nil, unity: "1,2", topics: "2,3,5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-4-numero-1-2009.html?p=11", category_id: 1, title: "“NA TRILHA DO SANGUE”: o jogo dos grupos sanguíneos", views: nil, unity: "1,2", topics: "2,3,5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-4-numero-1-2009.html?p=18", category_id: 4, title: "O “X” DA QUESTÃO: a diversidade gerada pela segregação cromossômica independente e a cor da pelagem de gatos", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-4-numero-2-2009.html?p=31", category_id: 1, title: "GENÉTICA NO COTIDIANO: o sistema ABO na transfusão sanguínea", views: nil, unity: "1,2", topics: "5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-1-2010.html?p=6", category_id: 1, title: "“CRUZAMENTOS MENDELIANOS”: o bingo das ervilhas", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-2-2010.html?p=38", category_id: 5, title: "BRINCANDO COM O SISTEMA SANGUÍNEO: proposta alternativa para o ensino dos grupos sanguíneos ABO", views: nil, unity: "1,2", topics: "5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-6-numero-1-ft3eomm9v.html?p=2", category_id: 3, title: "AÇÃO DE DOIS ANTÍGENOS: vai um refresco aí?", views: nil, unity: "1,2", topics: "5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-6-numero-2-2011.html?p=47", category_id: 1, title: "JOGO DA QUEIMADA: uma prática para o ensino da genética", views: nil, unity: "1,2", topics: "2,3,5,6,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-8-numero-2-2013.html?p=4", category_id: 4, title: "HEREDOGRAMAS DOS ESTUDANTES: das Anágrafes Paroquiais para a Escola", views: nil, unity: "1", topics: "5,6"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-8-numero-2-2013.html?p=22", category_id: 1, title: "“Tá ligado?”: uma forma lúdica de aprender ligação gênica", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-9-numero-1-2014.html?p=4", category_id: 3, title: "CANUDINHOS: uma simulação para aprender genética de populações e seleção natural", views: nil, unity: "1,2", topics: "2,3,5,8"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-9-numero-1-2014.html?p=32", category_id: 1, title: "DOMINÓ GÊNICO: interagindo para compreender a interação gênica", views: nil, unity: "1", topics: "2,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-9-numero-1-2014.html?p=48", category_id: 1, title: "O JOGO DA RESPOSTA AO DANO NO DNA", views: nil, unity: "2", topics: "9,10"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-10-numero-2.html?p=50", category_id: 4, title: "O HEREDOGRAMA NAS SÉRIES INICIAIS DO ENSINO FUNDAMENTAL", views: nil, unity: "1", topics: "2,5,6"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-3-numero-1-2008.html?p=2", category_id: 4, title: "ACONSELHAMENTO GENÉTICO: o exemplo do casamento entre primos", views: nil, unity: "1,2,4", topics: "2,3,5,6,8,11,16"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-3-numero-1-2008.html?p=34", category_id: 2, title: "QUESTÃO DE SENSIBILIDADE: um filme para conversar sobre a homossexualidade e conceitos básicos de genética", views: nil, unity: "2,3,4", topics: "11,12,14,16"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-4-numero-2-2009.html?p=37", category_id: 6, title: "GENÉTICA E ÉTICA EM PAINÉIS DIDÁTICOS", views: nil, unity: "2,4", topics: "7,16"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-1-2010.html?p=2", category_id: 3, title: "O TESTE DE DNA NA SALA DE AULA: é possível ensinar Biologia partir de temas atuais?", views: nil, unity: "1,3,4", topics: "3,5,14,16"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-1-2010.html?p=44", category_id: 4, title: "HIPERTENSÃO: uma herança genética multifatorial", views: nil, unity: "1,2,4", topics: "1,5,8,16"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-2-2010.html?p=2", category_id: 1, title: "“CONTÉM FENILALANINA, POSSO COMER?”", views: nil, unity: "1,2,3", topics: "5,8,13"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-5-numero-2-2010.html?p=24", category_id: 3, title: "CAÇANDO GENES", views: nil, unity: "1,2,3", topics: "3,5,6,8,11,12,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-6-numero-1-ft3eomm9v.html?p=5", category_id: 3, title: "UMA SIMULAÇÃO DE TESTE DE PATERNIDADE: quem é o pai do bezerro?", views: nil, unity: "1,3", topics: "5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-8-numero-1-2013.html?p=10", category_id: 3, title: "IDENTIFICANDO O SISTEMA DE ACASALAMENTO EM AVES", views: nil, unity: "1,3", topics: "3,5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-8-numero-1-2013.html?p=18", category_id: 4, title: "DISPERSÃO DO PÓLEN ENTRE PEQUIZEIROS: uma atividade para a genética no ensino superior", views: nil, unity: "1,3", topics: "3,5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-8-numero-1-2013.html?p=28", category_id: 4, title: "IDENTIFICAÇÃO DE ESPÉCIE E DO SEU LOCAL DE ORIGEM PARA A RESOLUÇÃO DE CRIMES CONTRA A FAUNA BRASILEIRA: uma atividade para o ensino de genética forense", views: nil, unity: "1,3", topics: "3,5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-9-numero-2-2014.html?p=28", category_id: 4, title: "AVALIAÇÃO DA DEPRESSÃO ENDOGÂMICA EM POPULAÇÕES DE ARATICUNZEIRO: apoio pedagógico ao Ensino de Genética", views: nil, unity: "1,3", topics: "3,5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-9-numero-2-2014.html?p=48", category_id: 4, title: "A GENÉTICA CONTRA OS CRIMES AMBIENTAIS: identificação de madeira ilegal provenientes de unidades de conservação utilizando marcador molecular", views: nil, unity: "1,3", topics: "3,5,14"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-11-numero-1.html?p=30", category_id: 2, title: "GUIA DO EDUCADOR PARA O FILME X-MEN PRIMEIRA CLASSE", views: nil, unity: "1", topics: "1,4,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-11-numero-1.html?p=48", category_id: 3, title: "SIMULANDO A RELAÇÃO ENTRE MUTAÇÃO E CÂNCER NA SALA DE AULA", views: nil, unity: "1,2", topics: "5,9,10"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-11-numero-2-suplemento.html?p=80", category_id: 1, title: "QUE ERVILHA SOU EU?", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-11-numero-2-suplemento.html?p=114", category_id: 1, title: "EMBARALHANDO MENDEL E SUAS LEIS", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-11-numero-2-suplemento.html?p=142", category_id: 1, title: "CARTEANDO COM MENDEL", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-gen-tica-na-escola-volume-12-n-mero-1.html?p=46", category_id: 4, title: "COLORAÇÃO DA PELAGEM CANINA: integrando conceitos básicos de genética clássica", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-gen-tica-na-escola-volume-12-n-mero-1.html?p=74", category_id: 4, title: "UTILIZAÇÃO DE MODELOS DE DROSOPHILA NO ESTUDO DE PADRÕES DE HERANÇA AUTOSSÔMICA E LIGADA AO SEXO", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-gen-tica-na-escola-volume-12-n-mero-1.html?p=90", category_id: 5, title: "MATERIAL DIDÁTICO PARA O ENSINO INCLUSIVO DE HERANÇA GENÉTICA", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-gen-tica-na-escola-volume-12-n-mero-2.html?p=66", category_id: 4, title: "OS GENES DE COLORAÇÃO MC1R E ASIP COMO MODELOS PARA O ENSINO DE GENÉTICA: dominância e interação gênica", views: nil, unity: "1", topics: "2,3,5"},
+  {link: "https://www.flipsnack.com/Eveli/revista-genetica-na-escola-volume-1-numero-2-2006.html?p=4", category_id: 2, title: "O ÓLEO DE LORENZO: O USO DO CINEMA PARA CONTEXTUALIZAR O ENSINO DE GENÉTICA E DISCUTIR A CONSTRUÇÃO DO CONHECIMENTO CIENTÍFICO", views: nil, unity: "1,2,3,4", topics: "2,5,6,8,11,12,16,17,18,19"}
+])

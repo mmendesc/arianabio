a481adc7328012ef9d8ca62bb8e83707bf3856ba
mmendesc <matheus.mendescf@gmail.com>
2018-06-19 22:13:32 -0300

    Change method to ability

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load',function(){
       url: '/search_by_title',
       data: {topic_id: $('select[name=topic_id]').val(),format: 'js'},
       success: function(){
-        $('.method-table-title').text('Métodos encontrados nas categorias abaixo')
+        $('.method-table-title').text('Habilidades encontradas nas categorias abaixo')
         $('.welcome .container').show();
         $("html, body").animate({ scrollTop: $('.welcome').offset().top }, 1000);
       }
@@ -18,7 +18,7 @@ $(document).on('turbolinks:load',function(){
       url: '/search_by_category',
       data: {title: $("input[name='category_id']").val(),format: 'js'},
       success: function(){
-        $('.method-table-title').text('Métodos encontrados nas categorias abaixo')
+        $('.method-table-title').text('Habilidades encontradas nas categorias abaixo')
         $('.welcome .container').show();
         $("html, body").animate({ scrollTop: $('.welcome').offset().top }, 1000);
       }
